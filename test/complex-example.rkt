0ed7e14725178f1155cd8694773f62e3f4482f3c
Summerlve <dzxx0563zh@126.com>
2020-03-12 12:00:55 +0800

    comment change

--- test/complex-example.rkt
+++ test/complex-example.rkt
@@ -1,13 +1,13 @@
 #lang racket
 (define a '(11 2.2 233 "abcd" "123abc"))
-(define v '(#t #f)); comment here
+(define v '(#t #f)) ; comment here
 (define c 2)
 a
 v
 c
 (let ([a (let ([d (/ (+ 1 100) 5)]) d)]
       [b (let* ([e (letrec ([f 1.5]) f)]) e)]
-      [c 3]) 1 (+ a b) (+ a (+ a b) c) #t (+ -11911.11 11.11)); let expression
+      [c 3]) 1 (+ a b) (+ a (+ a b) c) #t (+ -11911.11 11.11)) ; let expression
 (define plus (lambda (x y) (+ x y)))
 (plus 101 12)
 (plus 2 (plus 1 (plus (plus 81 2) (plus c 11))))
